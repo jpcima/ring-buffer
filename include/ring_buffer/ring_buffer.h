d5b9fe48e485a5836e29c1a4ff2769588c2d52ae
JP Cimalando <jpcima@users.noreply.github.com>
2018-04-05 23:11:30 +0200

    fix error in CRTP pattern

--- include/ring_buffer/ring_buffer.h
+++ include/ring_buffer/ring_buffer.h
@@ -34,7 +34,9 @@ typedef Ring_Buffer_Ex<true> Ring_Buffer;
 //------------------------------------------------------------------------------
 template <bool Atomic>
 class Ring_Buffer_Ex final :
-    private Basic_Ring_Buffer<Ring_Buffer> {
+    private Basic_Ring_Buffer<Ring_Buffer_Ex<Atomic>> {
+private:
+    typedef Basic_Ring_Buffer<Ring_Buffer_Ex<Atomic>> Base;
 public:
     // initialization and cleanup
     explicit Ring_Buffer_Ex(size_t capacity);
@@ -44,17 +46,17 @@ public:
     // read operations
     size_t size_used() const;
     bool discard(size_t len);
-    using Basic_Ring_Buffer::get;
-    using Basic_Ring_Buffer::peek;
+    using Base::get;
+    using Base::peek;
     // write operations
     size_t size_free() const;
-    using Basic_Ring_Buffer::put;
+    using Base::put;
 
 private:
     size_t cap_{0};
     std::conditional_t<Atomic, std::atomic<size_t>, size_t> rp_{0}, wp_{0};
     std::unique_ptr<uint8_t[]> rbdata_ {};
-    friend class Basic_Ring_Buffer;
+    friend Base;
     friend class Soft_Ring_Buffer;
     bool getbytes_(void *data, size_t len);
     bool peekbytes_(void *data, size_t len) const;
@@ -71,6 +73,8 @@ private:
 
 class Soft_Ring_Buffer final :
     private Basic_Ring_Buffer<Soft_Ring_Buffer> {
+private:
+    typedef Basic_Ring_Buffer<Soft_Ring_Buffer> Base;
 public:
     // initialization and cleanup
     explicit Soft_Ring_Buffer(size_t capacity);
@@ -80,11 +84,11 @@ public:
     // read operations
     size_t size_used() const;
     bool discard(size_t len);
-    using Basic_Ring_Buffer::get;
-    using Basic_Ring_Buffer::peek;
+    using Base::get;
+    using Base::peek;
     // write operations
     size_t size_free() const;
-    using Basic_Ring_Buffer::put;
+    using Base::put;
 
 private:
 #if !defined(RING_BUFFER_NO_STD_SHARED_MUTEX)
@@ -96,7 +100,7 @@ private:
 private:
     Ring_Buffer_Ex<false> rb_;
     mutable mutex_type shmutex_;
-    friend class Basic_Ring_Buffer;
+    friend Base;
     bool getbytes_(void *data, size_t len);
     bool peekbytes_(void *data, size_t len) const;
     bool putbytes_(const void *data, size_t len);
